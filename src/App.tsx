7c3d487f5759a1909a7d31668c07e61a30059780
Huseyn <huseyn.mikayil95@gmail.com>
2021-09-04 23:25:59 +0400

    Add css, image, svg loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
 import React from "react";
+import "./style.css";
 
 export const App = () => <h1>Hello World</h1>;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -17,10 +17,22 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
     ],
   },
   output: {
-    path: path.resolve(__dirname, "..", "../build"),
+    path: path.resolve(__dirname, "..", "./build"),
     filename: "bundle.js",
   },
   mode: "development",
