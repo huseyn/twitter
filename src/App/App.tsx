929799a94a5888da57a7756a48c004d9e34c71d8
Huseyn <huseyn.mikayil95@gmail.com>
2021-09-05 17:30:08 +0400

    Modify App component and and to its own folder

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import "./style.css";
+import "./App.css";
 
-export const App = () => {
-  const a = "ddsd";
+const App = () => {
   return <h1>{process.env.NODE_ENV}</h1>;
 };
+
+export default App;
